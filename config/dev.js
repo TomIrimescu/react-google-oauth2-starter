51ef26cf6e2ea7df98b244cfbf5c74baf64e4404
Tom Irimescu <tom@irimescu.com>
2020-07-08 10:10:29 -0400

    Update google credentials

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,10 @@
 module.exports = {
+  // googleClientID:
+  //   '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  // googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  '482644656111-4s121vk10k3f33dft2unva332do5ifog.apps.googleusercontent.com',
+googleClientSecret: 'dvRpVWobXHX-_tkAxVCfI1D3',
   // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
   mongoURI: 'mongodb://localhost/blog_dev',
   cookieKey: '123123123',
