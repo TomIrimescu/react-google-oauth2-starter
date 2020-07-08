db8d8453215f70f2fb097c932a557988236fc459
Tom Irimescu <tom@irimescu.com>
2020-07-08 11:29:07 -0400

    Refactor environment variables

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-  // googleClientID:
-  //   '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  // googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  googleClientID:
-  '482644656111-4s121vk10k3f33dft2unva332do5ifog.apps.googleusercontent.com',
-googleClientSecret: 'dvRpVWobXHX-_tkAxVCfI1D3',
-  // mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  mongoURI: 'mongodb://localhost/blog_dev',
-  cookieKey: '123123123',
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
 };
